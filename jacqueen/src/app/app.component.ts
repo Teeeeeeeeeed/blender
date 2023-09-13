6ed78b4fa45c52bb9f62b68d658a73a2c798c083
tedtelo <ted.te.lo.ttl@gmail.com>
2023-09-13 23:20:03 +1200

    init 3d assets

--- jacqueen/src/app/app.component.ts
+++ jacqueen/src/app/app.component.ts
@@ -1,10 +1,146 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import * as THREE from 'three';
+import { GLTF, GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { CSS2DRenderer } from 'three/examples/jsm/renderers/CSS2DRenderer.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'jacqueen';
+  model: any;
+  controls!: OrbitControls;
+
+  @ViewChild('canvas') private canvasRef!: ElementRef;
+
+  //* Cube Properties
+
+  @Input() public rotationSpeedX: number = 0.05;
+
+  @Input() public rotationSpeedY: number = 0.01;
+
+  @Input() public size: number = 200;
+
+  @Input() public texture!: string;
+
+  //* Stage Properties
+
+  @Input() public cameraZ: number = 400;
+
+  @Input() public fieldOfView: number = 1;
+
+  @Input('nearClipping') public nearClippingPlane: number = 1;
+
+  @Input('farClipping') public farClippingPlane: number = 1000;
+
+  //? Helper Properties (Private Properties);
+
+  private camera!: THREE.PerspectiveCamera;
+
+  private get canvas(): HTMLCanvasElement {
+    return this.canvasRef.nativeElement;
+  }
+  private loader = new GLTFLoader();
+  private geometry = new THREE.BoxGeometry(1, 1, 1);
+
+  private renderer!: THREE.WebGLRenderer;
+
+  private scene!: THREE.Scene;
+
+  /**
+   * Create the scene
+   *
+   * @private
+   * @memberof CubeComponent
+   */
+  private createScene() {
+    //* Scene
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0xffffff);
+    this.loader.load(
+      'assets/the-saturn-orbiter/source/model.gltf',
+      (gltf: GLTF) => {
+        this.model = gltf.scene.children[0];
+        var box = new THREE.Box3().setFromObject(this.model);
+        box.getCenter(this.model.position);
+        this.model.position.multiplyScalar(-1);
+        this.scene.add(this.model);
+        console.log('completed loading!');
+      }
+    );
+
+    //* Lights
+    const light = new THREE.AmbientLight(0xffffff, 1);
+    this.scene.add(light);
+
+    //*Camera
+    let aspectRatio = this.getAspectRatio();
+    this.camera = new THREE.PerspectiveCamera(
+      this.fieldOfView,
+      aspectRatio,
+      this.nearClippingPlane,
+      this.farClippingPlane
+    );
+    this.camera.position.z = this.cameraZ;
+  }
+
+  private getAspectRatio() {
+    return this.canvas.clientWidth / this.canvas.clientHeight;
+  }
+
+  /**
+   * Start the rendering loop
+   *
+   * @private
+   * @memberof CubeComponent
+   */
+  private startRenderingLoop() {
+    //* Renderer
+    // Use canvas element in template
+    this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas });
+    this.renderer.setPixelRatio(devicePixelRatio);
+    this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
+
+    let component: AppComponent = this;
+    (function render() {
+      requestAnimationFrame(render);
+      component.renderer.render(component.scene, component.camera);
+    })();
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  createControls() {
+    const renderer = new CSS2DRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.domElement.style.position = 'absolute';
+    renderer.domElement.style.top = '0px';
+
+    document.body.appendChild(renderer.domElement);
+
+    this.controls = new OrbitControls(this.camera, renderer.domElement);
+    this.controls.autoRotate = true;
+    this.controls.enableZoom = true;
+    this.controls.enablePan = true;
+    this.controls.target.set(0, 0, 0);
+    this.controls.update();
+  }
+
+  ngAfterViewInit() {
+    this.createScene();
+    this.startRenderingLoop();
+    this.createControls();
+  }
 }
